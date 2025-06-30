b7b6f9b33131ff31ea08da7cc0407c50f3642d2d
Aytac <abalayeva12@gmail.com>
2025-06-30 15:27:28 +0400

    Add build script

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-dotenv.config();
 import cookieParser from "cookie-parser";
 import express from "express";
 import taskRouter from "./routes/taskRouter.js";
@@ -8,23 +7,20 @@ import connectDB from "./config/db.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
-const app = express();
+dotenv.config();
 const port = process.env.PORT || 5000;
-
-//Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 // Connect to MongoDB
 connectDB();
 
-// Middleware to parse JSON
+const app = express();
+//Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON
 app.use(cookieParser());
 
 // Root route
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
 
 // Task API routes
 app.use("/api/tasks", taskRouter);
@@ -33,15 +29,23 @@ app.use("/api/users", userRoutes);
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
   });
 }
+
 // Start server
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
