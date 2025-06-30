e1944a47184c1f754f308e062a9fb330a189b19f
Aytac <abalayeva12@gmail.com>
2025-06-30 15:43:45 +0400

    Add build script

--- backend/server.js
+++ backend/server.js
@@ -29,21 +29,14 @@ app.use("/api/users", userRoutes);
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
 }
 
 // Start server
